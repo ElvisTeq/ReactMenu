c497d17b5a43b7fa39e16cd465db262513ea5396
elvis <cenelvis@gmail.com>
2022-05-29 01:08:47 -0400

    Cart Modal, using React Portal

---
+++ src/components/UI/Modal.js
@@ -0,0 +1,33 @@
+import React from "react";
+import classes from "./Modal.module.css";
+import ReactDOM from "react-dom";
+
+// Background when Cart is open
+const Backdrop = (props) => {
+  return <div className={classes.backdrop} />;
+};
+
+// Cart Model Overlay
+const ModalOverlay = (props) => {
+  return (
+    <div className={classes.modal}>
+      <div className={classes.content}>{props.children}</div>
+    </div>
+  );
+};
+
+const portalElement = document.getElementById("overlays");
+
+const Modal = (props) => {
+  return (
+    <React.Fragment>
+      {ReactDOM.createPortal(<Backdrop />, portalElement)}
+      {ReactDOM.createPortal(
+        <ModalOverlay>{props.children}</ModalOverlay>,
+        portalElement
+      )}
+    </React.Fragment>
+  );
+};
+
+export default Modal;
