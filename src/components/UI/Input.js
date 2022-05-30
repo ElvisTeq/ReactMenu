60349c9fd8989d7dd3f13d66a5ba2aa9ae9d450c
elvis <cenelvis@gmail.com>
2022-05-30 13:00:25 -0400

    Working with Refs, Context, to add Items

--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -2,14 +2,14 @@ import React from "react";
 import classes from "./Input.module.css";
 
 // htmlFor => To link <label> with <input> using the "props.input.id"
-// {...props.input} => contains "props.input.id" so they are linked
-const Input = (props) => {
+// {...props.input} => will contains "props.input.id" so they can be linked
+const Input = React.forwardRef((props, ref) => {
   return (
     <div className={classes.input}>
       <label htmlFor={props.input.id}>{props.label}</label>
-      <input {...props.input} />
+      <input ref={ref} {...props.input} />
     </div>
   );
-};
+});
 
 export default Input;
